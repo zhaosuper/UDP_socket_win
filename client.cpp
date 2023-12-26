933d04fdd76446fb1e36728e62faeaae5a89be45
chao <18810759115@163.com>
2023-12-26 15:35:14 +0800

    cpp

---
+++ client.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <chrono>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    // 初始化Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock.\n";
+        return 1;
+    }
+
+    // 创建UDP Socket
+    SOCKET clientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create socket.\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // 设置服务器地址
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // 服务器的IP地址，替换为你的服务器IP地址
+    serverAddr.sin_port = htons(11255);  // 选择一个固定端口，例如12345
+
+    // 获取起始时间
+    auto startTime = std::chrono::steady_clock::now();
+    
+    while (true) {
+
+        // 设置通信频率
+        if (std::chrono::steady_clock::now() - startTime >= std::chrono::milliseconds(50)) {
+            // 获取当前时间（ns单位）
+            auto now = std::chrono::system_clock::now().time_since_epoch().count();
+
+            // 发送当前时间
+            if (sendto(clientSocket, std::to_string(now).c_str(), std::to_string(now).size(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+                std::cerr << "sendto failed with error.\n";
+                closesocket(clientSocket);
+                WSACleanup();
+                return 1;
+            }
+
+            std::cout << "Sent time to server: " << now << std::endl;
+            // 重置起始时间
+            startTime = std::chrono::steady_clock::now();
+        }
+    }
+
+    // 关闭Socket和清理Winsock
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <fstream>
+#include <winsock2.h>
+#include <chrono>
+#include <ctime>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    // 初始化Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock.\n";
+        return 1;
+    }
+
+    // 创建UDP Socket
+    SOCKET serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Failed to create socket.\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // 设置服务器地址
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY; // 服务器的IP地址，替换为你的服务器IP地址
+    // serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(11255);  // 选择一个固定端口，例如12345
+
+    // 绑定Socket到端口
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed with error.\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "UDP Server is listening on port 11255...\n";
+
+    // 创建文件流
+    std::ofstream outputFile("transmission_delay.txt");
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open output file.\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // 接收数据
+    char buffer[1024];
+    sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+
+    while (true) {
+        int bytesReceived = recvfrom(serverSocket, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (bytesReceived == SOCKET_ERROR) {
+            std::cerr << "recvfrom failed with error.\n";
+            break;
+        }
+
+        buffer[bytesReceived] = '\0';
+        std::cout << "Received message from " << inet_ntoa(clientAddr.sin_addr) << ": " << buffer << std::endl;
+
+        // 解析接收到的时间信息
+        long long sentTime;
+        std::sscanf(buffer, "%lld", &sentTime);
+
+        // 计算传输时延
+        auto receiveTime = std::chrono::system_clock::now();
+        auto timeDifference = receiveTime.time_since_epoch().count() - sentTime;
+        // std::cout << "Transmission delay: " << timeDifference << " ms\n";
+
+
+        // 将时延转换为毫秒
+        auto timeDifferenceInMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::nanoseconds(timeDifference));
+        // 输出时延（毫秒）
+        std::cout << "Transmission delay: " << timeDifferenceInMilliseconds.count() << " ms\n";
+
+
+        // 将传输时延写入txt文件
+        outputFile << timeDifferenceInMilliseconds.count() << std::endl;
+        // outputFile << timeDifferenceInMilliseconds.count() << std::endl;
+    }
+
+    // 关闭Socket和清理Winsock
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
